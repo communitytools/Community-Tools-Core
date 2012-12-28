2ac0576feee457f7f33ccd77925e421b27a68218
Reigo <reigok@gmail.com>
2012-12-28 13:01:45 +0200

    Remake location title and name
    When editing a location the Full name and node Title are tied as was
    with the Location module.

--- js/cmtls.location_title.js
+++ js/cmtls.location_title.js
@@ -2,11 +2,11 @@
   Drupal.behaviors.cmtls_location_title = {
     attach: function (context, settings) {
       $('#edit-title').keyup(function () {
-        $('#edit-field-cmtls-location-und-0-name').val($(this).val());
+        $('#edit-field-cmtls-address-und-0-name-line').val($(this).val());
       });
-      $('#edit-field-cmtls-location-und-0-name').keyup(function () {
+      $('#edit-field-cmtls-address-und-0-name-line').keyup(function () {
         $('#edit-title').val($(this).val());
       });
     }
-  };  
+  };
 }(jQuery));
